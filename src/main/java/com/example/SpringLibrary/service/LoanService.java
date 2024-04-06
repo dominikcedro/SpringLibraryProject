ec875bcd2d443e906999036b44a7685a1c26ef89
Domin C <dominikcedro00@gmail.com>
2024-04-06 22:34:44 +0200

    ADD loan exceptions when book or user dont exist

--- src/main/java/com/example/SpringLibrary/service/LoanService.java
+++ src/main/java/com/example/SpringLibrary/service/LoanService.java
@@ -4,7 +4,10 @@ import com.example.SpringLibrary.dto.LoanDTO;
 import com.example.SpringLibrary.entity.Book;
 import com.example.SpringLibrary.entity.Loan;
 import com.example.SpringLibrary.entity.User;
+import com.example.SpringLibrary.exception.auth.UserNotExistingException;
+import com.example.SpringLibrary.exception.loan.IdUserNotExistingException;
 import com.example.SpringLibrary.exception.loan.LoanAlreadyExistingException;
+import com.example.SpringLibrary.exception.loan.LoanBookNotExistingException;
 import com.example.SpringLibrary.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,7 +40,17 @@ public class LoanService {
         if(existingLoan.isPresent()){
             throw LoanAlreadyExistingException.create(loanDTO.getBookId(), loanDTO.getUserId());
         }
+        // check if book for this loan with book id exists, if not throw exception
+        Optional<Loan> existingLoan1 = loanRepository.findByBookBookIdAndUserId(loanDTO.getBookId(), loanDTO.getUserId());
+        if(existingLoan1.isEmpty()){
+            throw LoanBookNotExistingException.create(loanDTO.getBookId());
+        }
 
+        // check wheter user with said id exists
+        Optional<User> existingUser = userService.getUserById(loanDTO.getUserId());
+        if(existingUser.isEmpty()){
+            throw IdUserNotExistingException.create(loanDTO.getUserId());
+        }
 
         Loan loan = new Loan();
         User user = userService.getUserById(loanDTO.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
